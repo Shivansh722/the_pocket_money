f8591dfe566628a8301dcbbb18ee30beda0faad1
Shivansh722 <sc5268@srmist.edu.in>
2024-07-28 04:36:35 +0530

    added fluency percent text

--- lib/src/presentation/screens/fln_pages/result.dart
+++ lib/src/presentation/screens/fln_pages/result.dart
@@ -10,23 +10,28 @@ class ResultMeterScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     double value;
     Color rangeColor;
+    String percentageText;
 
     switch (fluencyResult) {
       case 'green':
         value = 75;
         rangeColor = Colors.green;
+        percentageText = '75%';
         break;
       case 'yellow':
         value = 50;
         rangeColor = Colors.yellow;
+        percentageText = '50%';
         break;
       case 'red':
         value = 25;
         rangeColor = Colors.red;
+        percentageText = '25%';
         break;
       default:
         value = 0;
         rangeColor = Colors.grey;
+        percentageText = '0%';
     }
 
     return Scaffold(
@@ -34,39 +39,53 @@ class ResultMeterScreen extends StatelessWidget {
         title: Text('Fluency Result'),
       ),
       body: Center(
-        child: SfRadialGauge(
-          axes: <RadialAxis>[
-            RadialAxis(
-              minimum: 0,
-              maximum: 100,
-              ranges: <GaugeRange>[
-                GaugeRange(startValue: 0, endValue: 33, color: Colors.red, label: 'Low', sizeUnit: GaugeSizeUnit.factor, startWidth: 0.1, endWidth: 0.1),
-                GaugeRange(startValue: 33, endValue: 66, color: Colors.yellow, label: 'Medium', sizeUnit: GaugeSizeUnit.factor, startWidth: 0.1, endWidth: 0.1),
-                GaugeRange(startValue: 66, endValue: 100, color: Colors.green, label: 'High', sizeUnit: GaugeSizeUnit.factor, startWidth: 0.1, endWidth: 0.1),
-              ],
-              pointers: <GaugePointer>[
-                NeedlePointer(
-                  value: value,
-                  enableAnimation: true,
-                )
-              ],
-              annotations: <GaugeAnnotation>[
-                GaugeAnnotation(
-                  widget: Container(
-                    child: Text(
-                      fluencyResult,
-                      style: TextStyle(
-                        fontSize: 25,
-                        fontWeight: FontWeight.bold,
-                        color: rangeColor,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SfRadialGauge(
+              axes: <RadialAxis>[
+                RadialAxis(
+                  minimum: 0,
+                  maximum: 100,
+                  ranges: <GaugeRange>[
+                    GaugeRange(startValue: 0, endValue: 33, color: Colors.red, label: 'Low', sizeUnit: GaugeSizeUnit.factor, startWidth: 0.1, endWidth: 0.1),
+                    GaugeRange(startValue: 33, endValue: 66, color: Colors.yellow, label: 'Medium', sizeUnit: GaugeSizeUnit.factor, startWidth: 0.1, endWidth: 0.1),
+                    GaugeRange(startValue: 66, endValue: 100, color: Colors.green, label: 'High', sizeUnit: GaugeSizeUnit.factor, startWidth: 0.1, endWidth: 0.1),
+                  ],
+                  pointers: <GaugePointer>[
+                    NeedlePointer(
+                      value: value,
+                      enableAnimation: true,
+                    )
+                  ],
+                  annotations: <GaugeAnnotation>[
+                    GaugeAnnotation(
+                      widget: Container(
+                        child: Text(
+                          fluencyResult,
+                          style: TextStyle(
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold,
+                            color: rangeColor,
+                          ),
+                        ),
                       ),
-                    ),
-                  ),
-                  angle: 90,
-                  positionFactor: 0.5,
+                      angle: 90,
+                      positionFactor: 0.5,
+                    )
+                  ],
                 )
               ],
-            )
+            ),
+            SizedBox(height: 20), // Add space between gauge and text
+            Text(
+              percentageText,
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+                color: rangeColor,
+              ),
+            ),
           ],
         ),
       ),
